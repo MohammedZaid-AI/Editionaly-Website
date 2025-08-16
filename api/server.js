67512fd8e488acfc3f790591e1aeaa966a53abc4
Mohammed Zaid <149226252+MohammedZaid-AI@users.noreply.github.com>
2025-08-16 17:27:12 +0530

    itzzzz working after depolyment

--- api/server.js
+++ api/server.js
@@ -92,5 +92,6 @@ app.post('/razorpay-webhook', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
+//const PORT = process.env.PORT || 3001;
+// /app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
