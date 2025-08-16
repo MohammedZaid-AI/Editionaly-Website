d8e8ca33946ae62bdb0f4fbe0dafaaa92cf0b87d
Mohammed Zaid <149226252+MohammedZaid-AI@users.noreply.github.com>
2025-08-16 12:04:41 +0530

    itzzzz Working

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
+require('dotenv').config(); 
+
 const express = require('express');
 const Razorpay = require('razorpay');
 const cors = require('cors');
 const path = require('path');
 const { addRow, removeRow } = require('./sheets.js');
 
-require('dotenv').config(); // Load environment variables
 
 const app = express();
 app.use(cors());
